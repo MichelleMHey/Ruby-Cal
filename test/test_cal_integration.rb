c46ad0b7592cfbb8727b63e6fd946f7716d9d89c
MichelleMHey <michellemhey@gmail.com>
2013-04-26 11:58:42 -0500

    Second Commit

---
+++ test/test_cal_integration.rb
@@ -0,0 +1,18 @@
+require 'test/unit'
+
+class SampleMethods < Test::Unit::TestCase
+
+  def test_01_header_month_year
+  	
+  end
+
+  def test_02_day_header
+  	
+  end
+
+  def test_03_get_first_of_month
+  	
+  end
+
+
+end
