46603938e7712b87e9f581e0eff4634d574cee09
MichelleMHey <michellemhey@gmail.com>
2013-04-28 16:15:49 -0500

    not finished

---
+++ lib/cal.rb
@@ -0,0 +1,35 @@
+class Calendar
+
+  WEEKDAY = "Su, Mo, Tu, We, Th, Fr, Sa"
+  attr_reader :month
+  attr_reader :year
+
+  def initialize(month, year)
+  	
+  end
+
+  def month_head
+  
+  end
+
+  def days_of_week
+  	
+  end
+
+  def is_leap_year?
+  	
+  end
+
+  def start_day_of_month
+  	
+  end
+
+  def to_s
+  	
+  end
+
+  def num_of_days
+  	
+  end
+
+end
