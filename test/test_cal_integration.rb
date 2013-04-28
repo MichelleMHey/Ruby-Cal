b29de2079f6feb289b5afddfe96d1213ff05d85f
MichelleMHey <michellemhey@gmail.com>
2013-04-28 16:16:12 -0500

    not finished

--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -1,17 +1,18 @@
 require 'test/unit'
+require 'cal'
 
-class SampleMethods < Test::Unit::TestCase
+class TestCal < Test::Unit::TestCase
 
   def test_01_header_month_year
-  	
-  end
+    feb = `cal 2 2013`.split(/\n/).first.strip!
+    assert_equal(feb, Calendar.new(2, 2013).month_head)
+  end 
 
-  def test_02_day_header
-  	
+  def test_02_days_of_week_header
+  	week_day = Calendar::WEEKDAY
   end
 
   def test_03_get_first_of_month
-  	
   end
 
 
